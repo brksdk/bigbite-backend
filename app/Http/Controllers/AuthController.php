1caf537117b383853fcec241bce0422b6d146d6f
Berk Sadikoglu <wowitsberk@gmail.com>
2021-12-08 21:59:47 +0300

    sea

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,18 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function admin(Request $request)
     {
 
-        $user = new User();
-        $user->name=$request->post('name');
-        $user->email=$request->post('email');
-        $user->password=$request->post('password');
-        if($user->save())
+        $users = new User();
+        $users->name=$request->post('name');
+        $users->email=$request->post('email');
+        $users->password=$request->post('password');
+        if($users->save())
         {
             return response()->json(['succes'=>true
             ]);
@@ -26,4 +27,28 @@ class AuthController extends Controller
         }
 
     }
+
+    public function register(Request $request)
+    {
+
+        $customers = new Customer();
+        $customers->firstName=$request->post('firstName');
+        $customers->lastName=$request->post('lastName');
+        $customers->email=$request->post('email');
+        $customers->phoneNumber=$request->post('phoneNumber');
+        $customers->password=$request->post('password');
+        if($customers->save())
+        {
+            return response()->json(['succes'=>true
+            ]);
+
+        }else{
+            return response()->json([
+                'succes'=>false
+            ]);
+        }
+
+    }
+
+
 }
