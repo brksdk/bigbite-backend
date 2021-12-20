aadcf2c18499833085bb7ddf5e58e1d9bf297e82
Berk Sadikoglu <wowitsberk@gmail.com>
2021-12-20 19:12:08 +0300

    deneme

--- resources/views/admin/orders.blade.php
+++ resources/views/admin/orders.blade.php
@@ -1,32 +1,3 @@
-<<<<<<< HEAD
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Admin Panel Siparişler</title>
-</head>
-<body>
-
-<table>
-    <tr>
-        <th>Ad</th>
-        <th>Soyad</th>
-        <th>Email</th>
-    </tr>
-    @foreach($customers as $customer)
-    <tr>
-        <td>{{$customer->firstName}}</td>
-        <td>{{$customer->lastName}}</td>
-        <td>{{$customer->email}}</td>
-
-    </tr>
-    @endforeach
-
-</table>
-=======
 
 <!DOCTYPE html>
 <html lang="en">
@@ -70,6 +41,5 @@
 
 </div>
 @include("admin.adminscript")
->>>>>>> 4d61e85 (with admin panel)
 </body>
 </html>
